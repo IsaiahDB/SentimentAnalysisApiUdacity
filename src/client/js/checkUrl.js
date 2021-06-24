231a7d895eddf50e162bc38891cdfa7ac5d7532f
IsiahDB <IsaiahBanks500@gmail.com>
2021-06-24 11:13:17 -0500

    commit

--- src/client/js/checkUrl.js
+++ src/client/js/checkUrl.js
@@ -1,7 +1,7 @@
 export const urlChecker = (articleUrl) => {
     let check = articleUrl.match(/^(ftp|http|https):\/\/[^ "]+$/);
     if (check === null){
-        alert("Please enter a valid URL beginning with http:// or https://")
+        alert("Enter a website with http:// or https://")
     } else {
     return true }
 }
