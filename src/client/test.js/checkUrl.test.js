a513e77169141221c831f74b40c2bec7c883736d
IsiahDB <IsaiahBanks500@gmail.com>
2021-06-25 12:18:04 -0500

    commit

---
+++ src/client/test.js/checkUrl.test.js
@@ -0,0 +1,7 @@
+
+const handleSubmit = require('./js/formHandler');
+//import { handleSubmit } from './js/formHandler'
+
+test('This test should return true', () => {
+    expect(urlChecker().toBeTrue(true))
+})
---
+++ src/client/test.js/handleSubmit.test.js
@@ -0,0 +1,9 @@
+
+const handleSubmit = require('./js/formHandler');
+//import { handleSubmit } from './js/formHandler'
+
+describe("The URL should start with a HTTP OR HTTS." , () => {
+    const httpMatcher = "";
+    expect.stringMatching(handleSubmit(httpMatcher)).toMatch(httpMatcher)
+})
+
